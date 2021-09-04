7d3c42a2a80519f7e8c8b98249718d46fbdf4195
Nicole Tietz-Sokolskaya <me@ntietz.com>
2021-09-03 21:33:07 -0400

    Aw, how cute, my very first quine

---
+++ quine.py
@@ -0,0 +1,6 @@
+first = "first = "
+second = '\nprint(first + "\\"" + first + "\\"")\nprint("second = " + repr(second))\nprint(second)'
+
+print(first + "\"" + first + "\"")
+print("second = " + repr(second))
+print(second)
